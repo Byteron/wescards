300d0bec0b4a6b28396dd396392786d936430095
Aaron Winter <winter.aaron93@gmail.com>
2019-11-04 19:47:26 +0100

    AI rework

--- source/map/Tile.gd
+++ source/map/Tile.gd
@@ -20,10 +20,22 @@ func _ready() -> void:
 	if is_village:
 		flag_label.text = "+%d" % income
 		flag.show()
-
 func capture(team, team_color):
 	if not is_village:
 		return
 
 	flag.self_modulate = team_color
 	emit_signal("captured", team, cell)
+
+func get_danger_level(team):
+	var danger_level = 0
+	for n_tile in neighbors:
+		if n_tile.unit and n_tile.unit.team != team:
+			danger_level += 1
+	return danger_level
+
+func is_save_for_team(team):
+	for n_tile in neighbors:
+		if n_tile.unit and n_tile.unit.team != team:
+			return false
+	return true
--- source/match/AI.gd
+++ source/match/AI.gd
@@ -1,41 +1,134 @@
 extends Node
 class_name AI
 
+signal action_taken
+signal cards_played
 signal turn_finished
-signal combatting_units_finished
-signal moving_units_finished
-signal played_card
+
+func get_actions_dict():
+	return {
+		"move": { "value": 0, "tile": null},
+		"combat": { "value": 0, "unit": null},
+		"retreat": { "value": 0, "tile": null},
+		"rest": { "value": 2 },
+	}
 
 func make_turn(game, player):
 	randomize()
-	_combat_units(game, player)
-	yield(self, "combatting_units_finished")
-	_move_units(game, player)
-	yield(self, "moving_units_finished")
+
+	# move units
+	for unit in player.units:
+		for i in unit.actions:
+			if not unit.actions:
+				continue
+			var actions = get_actions_dict()
+			_evaluate_retreat(unit, actions)
+			_evaluate_combat(unit, actions)
+			_evaluate_move(unit, actions)
+			call_deferred("_take_action", game, unit, actions)
+			yield(self, "action_taken")
+
 	_play_cards(game, player)
-	yield(self, "played_card")
-	call_deferred("emit_signal", "turn_finished")
+	yield(self, "cards_played")
 
-func _combat_units(game, player):
-	for unit in player.units:
-		var tile = _get_combat_tile(unit, game)
-		if tile:
-			game.combat(unit, tile.unit)
-			if game:
-				yield(game, "combat_finished")
-	call_deferred("emit_signal", "combatting_units_finished")
+	emit_signal("turn_finished")
 
-func _move_units(game, player):
-	for unit in player.units:
+func _evaluate_retreat(unit, actions):
+	# if not badly hurt, don't retreat
+	if unit.health.value > unit.health.maximum / 2:
+		return
 
-		if not unit.actions:
-			continue
+	#otherwise
+	# if already save, rest
+	if unit.tile.is_save_for_team(unit.team):
+		actions.rest.value += 10
+		# if not save, move to other tile
+	else:
+		var n_tile = _get_savest_tile(unit)
 
-		var tile = _get_move_tile(unit, game)
-		if tile:
-			game.move_unit(unit, tile)
-			yield(game.tween, "tween_completed")
-	call_deferred("emit_signal", "moving_units_finished")
+		# check if there is saver tile, if so, move, if not, rest
+		if not n_tile or n_tile.get_danger_level(unit.team) >= unit.tile.get_danger_level(unit.team):
+			actions.rest.value += 10
+		else:
+			actions.retreat.value += 12
+			actions.retreat.tile = n_tile
+
+func _get_savest_tile(unit):
+	var free_tiles := []
+
+	for n_tile in unit.tile.neighbors:
+		if not n_tile.unit:
+			free_tiles.append(n_tile)
+
+	if not free_tiles:
+		return null
+
+	var save_tile = free_tiles[randi() % free_tiles.size()]
+
+	for f_tile in free_tiles:
+		if f_tile.is_save_for_team(unit.team):
+			return f_tile
+		elif f_tile.get_danger_level(unit.team) < save_tile.get_danger_level(unit.team):
+			save_tile = f_tile
+
+	return save_tile
+
+func _evaluate_combat(unit, actions):
+	var combat_tiles := []
+	for n_tile in unit.tile.neighbors:
+		if n_tile.unit and n_tile.unit.team != unit.team:
+			combat_tiles.append(n_tile)
+
+	if not combat_tiles:
+		return
+
+	actions["combat"].value = 8
+	actions["combat"].unit = combat_tiles[randi() % combat_tiles.size()].unit
+
+func _evaluate_move(unit, actions):
+	var move_tiles := []
+	var village_tiles := []
+
+	for n_tile in unit.tile.neighbors:
+		if not n_tile.unit and n_tile.get_danger_level(unit.team):
+			move_tiles.append(n_tile)
+
+	for m_tile in move_tiles:
+		if m_tile.is_village:
+			village_tiles.append(m_tile)
+
+	if village_tiles:
+		actions["move"].value = 9
+		actions["move"].tile = village_tiles[randi() % village_tiles.size()]
+		return
+
+	if move_tiles:
+		actions["move"].value = 6
+		actions["move"].tile = move_tiles[randi() % move_tiles.size()]
+
+
+func _take_action(game, unit, actions: Dictionary):
+	var a_key = "rest"
+
+	# get best action
+	for key in actions.keys():
+		if actions[a_key].value < actions[key].value:
+			a_key = key
+
+	match a_key:
+		"retreat":
+			game.move_unit(unit, actions["retreat"].tile)
+			yield(game.tween, "tween_all_completed")
+		"move":
+			game.move_unit(unit, actions["move"].tile)
+			yield(game.tween, "tween_all_completed")
+		"combat":
+			game.combat(unit, actions["combat"].unit)
+			yield(game, "combat_finished")
+		"rest":
+			pass
+
+	call_deferred("emit_signal", "action_taken")
 
 func _play_cards(game, player):
 	for card in player.hand:
@@ -52,69 +145,13 @@ func _play_cards(game, player):
 					game.place_land(card, tile, tile.rect_global_position)
 
 				yield(get_tree().create_timer(game.ANIMATION_TIME), "timeout")
-	call_deferred("emit_signal", "played_card")
-
-func _get_combat_tile(unit, game):
-	var tiles = []
-	var combat_tiles = []
-	var tile = null
-
-	# get all tiles with enemy units
-	for n_tile in unit.tile.neighbors:
-		if n_tile.unit and n_tile.unit.team != unit.team:
-			tiles.append(n_tile)
-
-	# filter for units that won't kill the attacker
-	for tile in tiles:
-		if _shall_attack(unit, tile.unit):
-			combat_tiles.append(tile)
-
-	# pick random tile
-	if combat_tiles:
-		tile = combat_tiles[0]
-		# pick the weakest or the leader
-		for c_tile in combat_tiles:
-			# return hero
-			if c_tile.unit.is_hero:
-				return c_tile
-			# set to tile if weaker then previous tile
-			if c_tile.unit.health.value < tile.unit.health.value:
-				tile = c_tile
-
-	return tile
-
-func _shall_attack(unit1, unit2):
-	if unit1.ranged.value > 0:
-		return true
-
-	var shall_attack = true
-	shall_attack = shall_attack and unit2.melee.value - unit1.armor.value < unit1.health.value
-	shall_attack = shall_attack and unit2.melee.value - unit1.armor.value <= unit1.melee.value - unit2.armor.value
-	return shall_attack
-
-func _get_move_tile(unit, game):
-	var tile = null
-	var tiles = []
-	var villages = []
-
-	for n_tile in unit.tile.neighbors:
-		if not n_tile.unit:
-			tiles.append(n_tile)
-
-	for n_tile in tiles:
-		if n_tile.is_village:
-			villages.append(n_tile)
-
-	if villages:
-		tile = villages[randi() % villages.size()]
-	elif tiles:
-		tile = tiles[randi() % tiles.size()]
-
-	return tile
+	call_deferred("emit_signal", "cards_played")
 
 func _get_free_nonland_tile(player):
 	for tile in player.get_summon_tiles():
 		if not tile.land:
+			if tile.unit and tile.unit.team != player.team:
+				continue
 			return tile
 	return null
 
