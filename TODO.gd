3f62027cb6362b6f45d22108f9248af8bfc251ef
Aaron Winter <winter.aaron93@gmail.com>
2019-11-07 14:02:47 +0100

    update todo

--- TODO.gd
+++ TODO.gd
@@ -4,14 +4,6 @@ In-Game Deckbuilder
 
 Projectiles
 
-Land Cards!
-	- Cards than can be played onto a tile
-		- Fortress +1 Armor
-		- Shrine + 2 Heal
-
-Advancements
-	- when a Unit get's experience, it shall advance to a better version
-
 Magical & Physical Attacks
 	- Magical Attacks ignore Armor
 
@@ -19,8 +11,6 @@ Unit Abilities
 	- Heal, Poison, etc
 
 Spells!
-	- Deal X Damage
-	- Heal X Health
 	- Firststrike
 	- Swap
 
